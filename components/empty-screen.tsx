35045129914b8e328d5271ac4aff5f83a4a61004
Ian Dalmas <ian.dalmas@icloud.com>
2024-05-15 14:21:09 -0500

    fix maybe?

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -14,6 +14,11 @@ export function EmptyScreen() {
         <p className="leading-normal text-muted-foreground">
           This is an open source life coach AI designed to help users live happy and healthy lives. Developed by the Teenfully team. 
         </p>
+        <p className="leading-normal text-muted-foreground">
+          We hope you enjoy!{' '}
+          {' '}
+          
+        </p>
       </div>
     </div>
   )
